8483202b6672178b9e2dcd9e71bcd6e359d15c7f
Sachin Chakrawarti <sachinchakrawarti69@gmail.com>
2026-07-20 12:51:39 +0530

    [#71] 🐍 Update 3 Python code

---
+++ disciplines/commerce/futures_n_options/ethereum/python/analysis/load_data.py
@@ -0,0 +1,65 @@
+import sqlite3
+import pandas as pd
+
+
+DATABASE = "../../database/ethereum.db"
+
+
+def load_ohlcv():
+
+    connection = sqlite3.connect(
+        DATABASE
+    )
+
+
+    query = """
+
+    SELECT
+        open_time,
+        open,
+        high,
+        low,
+        close,
+        volume
+
+    FROM market_ohlcv
+
+    ORDER BY open_time
+
+    """
+
+
+    df = pd.read_sql_query(
+        query,
+        connection
+    )
+
+
+    connection.close()
+
+
+    df["date"] = pd.to_datetime(
+        df["open_time"],
+        unit="ms"
+    )
+
+
+    df.set_index(
+        "date",
+        inplace=True
+    )
+
+
+    return df
+
+
+
+if __name__ == "__main__":
+
+    data = load_ohlcv()
+
+    print(data.head())
+
+    print()
+
+    print(data.tail())
