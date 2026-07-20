b286b5c2428913fb6b477e3a4cf1f3bcb4997050
Sachin Chakrawarti <sachinchakrawarti69@gmail.com>
2026-07-20 12:14:42 +0530

    [#16] ⚡ Update 3 JavaScript code

---
+++ disciplines/commerce/futures_n_options/ethereum/api/binance/utils/constants.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    SYMBOL: "ETHUSDT",
+
+    DATABASE: "ethereum.db",
+
+    REPORT_FOLDER: "reports",
+
+    BACKUP_FOLDER: "database/backups",
+
+    DEFAULT_LIMIT: 1000
+
+};
