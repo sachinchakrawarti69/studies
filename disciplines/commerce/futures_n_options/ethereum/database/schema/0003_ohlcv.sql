30cf92e40e4eadf96f3b5d4ab5925e802e4abbfd
Sachin Chakrawarti <sachinchakrawarti69@gmail.com>
2026-07-20 12:20:46 +0530

    [#28] 📁 Update 3 .sql

--- disciplines/commerce/futures_n_options/ethereum/database/schema/0003_ohlcv.sql
+++ disciplines/commerce/futures_n_options/ethereum/database/schema/0003_ohlcv.sql
@@ -2,6 +2,10 @@ CREATE TABLE IF NOT EXISTS market_ohlcv (
 
     id INTEGER PRIMARY KEY AUTOINCREMENT,
 
+    symbol TEXT DEFAULT 'ETHUSDT',
+
+    interval TEXT DEFAULT '1d',
+
     open_time INTEGER UNIQUE,
 
     open REAL,
@@ -22,6 +26,8 @@ CREATE TABLE IF NOT EXISTS market_ohlcv (
 
     taker_buy_base_volume REAL,
 
-    taker_buy_quote_volume REAL
+    taker_buy_quote_volume REAL,
+
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 
 );
