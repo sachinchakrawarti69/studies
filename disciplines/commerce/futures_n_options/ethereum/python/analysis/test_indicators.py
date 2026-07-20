f010a301004f45705a3f555b529b847edcc8d18f
Sachin Chakrawarti <sachinchakrawarti69@gmail.com>
2026-07-20 13:10:20 +0530

    [#112] 🐍 Update 3 Python code

--- disciplines/commerce/futures_n_options/ethereum/python/analysis/test_indicators.py
+++ disciplines/commerce/futures_n_options/ethereum/python/analysis/test_indicators.py
@@ -1,6 +1,3 @@
-from load_data import load_ohlcv
-from indicator_pipeline import apply_indicators
-
 import os
 
 
@@ -33,22 +30,3 @@ df.to_csv(
 
 print("Saved:")
 print(DATASET_PATH)
-
-df = load_ohlcv()
-
-
-
-df = apply_indicators(df)
-
-
-
-print(df.tail())
-
-
-
-df.to_csv(
-    "../../datasets/ethereum_indicators.csv"
-)
-
-
-print("Indicators completed")
