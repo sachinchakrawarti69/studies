5e2a4cf2717a270fc01a958f9f3b814800342e1b
Sachin Chakrawarti <sachinchakrawarti69@gmail.com>
2026-07-20 12:54:46 +0530

    [#76] 🐍 Update 3 Python code

---
+++ disciplines/commerce/futures_n_options/ethereum/python/charts/price_chart.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+
+from analysis.load_data import load_ohlcv
+
+
+df = load_ohlcv()
+
+
+plt.figure(figsize=(14,6))
+
+
+plt.plot(
+    df.index,
+    df["close"]
+)
+
+
+plt.title(
+    "Ethereum ETHUSDT Historical Price"
+)
+
+
+plt.xlabel(
+    "Date"
+)
+
+
+plt.ylabel(
+    "Price USDT"
+)
+
+
+plt.grid()
+
+
+plt.tight_layout()
+
+
+plt.savefig(
+    "../../charts/daily/eth_price_history.png",
+    dpi=300
+)
+
+
+plt.close()
+
+
+print(
+    "Price chart created"
+)
