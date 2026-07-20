b9f65f0d207683cea6ac7654d9a2495e176b8264
Sachin Chakrawarti <sachinchakrawarti69@gmail.com>
2026-07-20 13:10:11 +0530

    [#111] 🐍 Update 3 Python code

--- disciplines/commerce/futures_n_options/ethereum/python/analysis/test_indicators.py
+++ disciplines/commerce/futures_n_options/ethereum/python/analysis/test_indicators.py
@@ -1,7 +1,38 @@
 from load_data import load_ohlcv
 from indicator_pipeline import apply_indicators
 
+import os
 
+
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(
+            os.path.abspath(__file__)
+        )
+    )
+)
+
+
+DATASET_PATH = os.path.join(
+    BASE_DIR,
+    "datasets",
+    "ethereum_indicators.csv"
+)
+
+
+os.makedirs(
+    os.path.dirname(DATASET_PATH),
+    exist_ok=True
+)
+
+
+df.to_csv(
+    DATASET_PATH
+)
+
+
+print("Saved:")
+print(DATASET_PATH)
 
 df = load_ohlcv()
 
